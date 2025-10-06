e89ba0109e6fb788b35685319165afd1a80b7227
sifat-samin <sifatsamin19@gmail.com>
2025-10-07 02:18:05 +0600

    Input Text Correctness Check and Timer Dynamic added

--- Speed Typing Game/js/custom.js
+++ Speed Typing Game/js/custom.js
@@ -1,6 +1,36 @@
 const Random_Text_API = 'http://api.quotable.io/random';
 const textDisplay = document.getElementById('textDisplay');
 const textInput = document.getElementById('textInput');
+const timer = document.getElementById('timer');
+
+textInput.addEventListener('input', () => {
+    const textCharacterArray = document.querySelectorAll('span');
+    const typedTexts = textInput.value.split('');
+    let check = true;
+
+    textCharacterArray.forEach((chSpan, idx) => {
+        const character = typedTexts[idx];
+
+        if(character == null){
+            chSpan.classList.remove('correct');
+            chSpan.classList.remove('incorrect');
+            check = false;
+        }
+
+        else if(character === chSpan.innerText){
+            chSpan.classList.add('correct');
+            chSpan.classList.remove('incorrect');
+        }
+
+        else{
+            chSpan.classList.remove('correct');
+            chSpan.classList.add('incorrect');
+            check = false;
+        }
+    })
+
+    if(check) renderNewTexts();
+})
 
 const getRandomTexts = () => {
     return fetch(Random_Text_API)
@@ -18,11 +48,24 @@ const renderNewTexts = async () => {
     textCharacterArray.forEach(ch => {
         const chSpan = document.createElement('span');
         chSpan.innerText = ch;
-
         textDisplay.appendChild(chSpan);
     })
 
     textInput.value = null;
+    startTimer();
 }
 
+let startTime;
+const startTimer = () => {
+    timer.innerText = 0;
+    startTime = new Date();
+
+    setInterval(() => {
+        timer.innerText = getTimer();
+    }, 1000)
+}
+
+const getTimer = () => {
+    return Math.floor((new Date() - startTime) / 1000);
+}
 renderNewTexts();
