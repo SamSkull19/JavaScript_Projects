b2b1a7a9c56f963cfb8b2b7562d7db558d0a2ec6
sifat-samin <sifatsamin19@gmail.com>
2026-01-22 21:23:29 +0600

    Age Calculation JS code added

--- Glowing Age Calculator/script.js
+++ Glowing Age Calculator/script.js
@@ -92,13 +92,66 @@ function toggleTheme() {
 window.onload = () => {
     const savedTheme = localStorage.getItem("theme");
 
-    if(savedTheme === "dark") {
+    if (savedTheme === "dark") {
         document.body.classList.add("dark-theme");
         document.getElementById("checkbox").checked = true;
     }
 };
 
 
+const calculateBTN = document.querySelector('ripple_btn');
+checkboxInput.addEventListener('click', calculateAge);
+
+
+function calculateAge(e) {
+    const btn = e.currentTarget;
+    const ripple = document.createElement('span');
+    const rect = btn.getBoundingClientRect();
+
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    ripple.classList.add('ripple');
+    ripple.style.left = `${x}px`;
+    ripple.style.top = `${y}px`;
+
+    btn.appendChild(ripple);
+
+    setTimeout(() => ripple.remove(), 600);
+
+
+    const birthDateValue = document.getElementById("dateInput").value;
+    const result = document.getElementById("result");
+
+    if (!birthDateValue) {
+        result.innerHTML = "Select your birthday!";
+        return;
+    }
+
+    const birthDate = new Date(birthDateValue);
+    const today = new Date();
+
+
+    let year = today.getFullYear() - birthDate.getFullYear();
+    let month = today.getMonth() - birthDate.getMonth();
+    let day = today.getDate() - birthDate.getDate();
+
+    if (day < 0) {
+        month--;
+        day += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+    }
+
+    if (month < 0) {
+        year--;
+        month += 12;
+    }
+
+    result.classList.remove('animate_result');
+    void result.offsetWidth;
+    result.classList.add('animate_result');
+
+    result.innerHTML = `You are <br><span class="result-highlight">${year}y ${month}m ${day}d</span><br> old`;
+}
 
 
 
